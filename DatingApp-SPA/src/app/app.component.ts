82e925359752a96feb1a55782600b5ff89ab2bf6
Benjamin Aragon <baragon23@gmail.com>
2020-01-17 18:37:30 -0800

    Routing and Authorization implemented

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
@@ -7,7 +7,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {	
+export class AppComponent implements OnInit {
 	jwtHelper = new JwtHelperService();
 	
 	constructor(private authService: AuthService) {
